fd4cd8bbdc59afb0e443364211eed6c21e8727f3
kev gullyt <kevingullytps@gmail.com>
2023-02-17 23:47:48 +0000

    "Initial commit"

--- aulas/boleano.py
+++ aulas/boleano.py
@@ -1,4 +1,13 @@
-# para criar uma variavel do tipo Boleano (bool) conhecido tambem como bolean
+#entrada = input("O dia está chuvoso?\nSim ou Não\n\nEntrada: ")
 
-chuva = True
-ferias = False
+nome_usuario = input("Informe seu nome:\nEntrada: ")
+sobrenome_usuario = input("\nInforme seu sobrenome:\nEntrada: ")
+
+
+def saudar_usuario(nome, sobrenome):
+    return f"\nBem-vindo mestre {nome} {sobrenome}"
+
+
+print(
+    saudar_usuario(nome_usuario, sobrenome_usuario)
+)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ print(n1.isnumeric())
 
 variavel1 = 'Texto'     # Tipo texto  (string) str
 variavel2 = 2           # Tipo numerico inteiro (number) int
-variavel3 = 23.5        # Tipo numerico Real (float) float 
+variavel3 = 23.5        # Tipo numerico Real (float) float
 
 variavel = True         # Tipo logico, Verdadeiro ou Falso (Bolean) bool
+
