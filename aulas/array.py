04db0b7c353291b01deeb4a3be7d31b1b3d3bd84
kev gullyt <kevingullytps@gmail.com>
2023-02-19 02:20:14 +0000

    array com for sem sucesso

---
+++ aulas/array.py
@@ -0,0 +1,13 @@
+import sys
+
+bag = []
+bag.append('master ball')
+bag.append('Zangetsu')
+# print(bag)
+
+
+# Not Working ???????????????
+numbers = [2, 2, 2, 2, 2]
+for item in numbers:
+    s = item + 2
+    print(s)
